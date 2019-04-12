2f703b54f02a29d10ec11a34979a89f22f305d9a
J Shim <shimjay1@gmail.com>
2019-04-12 15:51:10 -0400

    CAL-18 Assessment Controller: delete assessment

--- AssessmentService/src/main/java/com/revature/caliber/controllers/AssessmentController.java
+++ AssessmentService/src/main/java/com/revature/caliber/controllers/AssessmentController.java
@@ -2,11 +2,21 @@ package com.revature.caliber.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.caliber.beans.Assessment;
@@ -32,5 +42,12 @@ public class AssessmentController {
         log.debug("Inside findAssessmentById");
         return as.findAssessmentById(id);
     }
+    
+    @DeleteMapping(value="all/assessment/delete", consumes=MediaType.APPLICATION_JSON_VALUE)
+	@Transactional(isolation=Isolation.READ_COMMITTED, propagation=Propagation.REQUIRED)
+	public ResponseEntity<Void> deleteLocation(@Valid @RequestBody Assessment assessment) {
+		as.deleteAssessment(assessment);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
     
 }
