80ab6eabaaed74e285414b7dd8a031ec884a3c91
Kevin Tran Huu <kevin.tranhuu@gmail.com>
2019-04-14 09:07:49 -0400

    Fixed conflicts

--- AssessmentService/src/main/java/com/revature/caliber/services/AssessmentServiceInterface.java
+++ AssessmentService/src/main/java/com/revature/caliber/services/AssessmentServiceInterface.java
@@ -10,12 +10,9 @@ import com.revature.caliber.beans.Assessment;
 public interface AssessmentServiceInterface {
 	public List<Assessment> findAllAssessments();
 	public Assessment findAssessmentById(Integer id);
-<<<<<<< HEAD
 	public float calculateAverageByAssessment(Integer batchId, Integer assessmentId);
-=======
 	public Boolean deleteAssessment(Assessment as);
 	public Assessment createAssessment(Assessment as);
 	public Assessment updateAssessment(Assessment as);
 
->>>>>>> 984da9d9116b76ab5fcfda8256f5f1637a3c04c3
 }
