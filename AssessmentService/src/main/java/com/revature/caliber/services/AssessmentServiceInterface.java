86c8993df3fad73b327a5a8daaaab79292f824ec
Kelby Behounek <knbehounek@hotmail.com>
2019-04-17 10:21:26 -0400

    fixing dev merge issues

--- AssessmentService/src/main/java/com/revature/caliber/services/AssessmentServiceInterface.java
+++ AssessmentService/src/main/java/com/revature/caliber/services/AssessmentServiceInterface.java
@@ -15,5 +15,4 @@ public interface AssessmentServiceInterface {
 	public Assessment updateAssessment(Assessment as);
 	public List<Assessment> findAssessmentsByBatchId(Integer batchId);
 	public List<Assessment> findAssessmentsByCategory(Integer categoryId);
-	public List<Assessment> findAssessmentsByBatchIdAndWeekNumber(Integer id, Integer weekNumber);
 }
