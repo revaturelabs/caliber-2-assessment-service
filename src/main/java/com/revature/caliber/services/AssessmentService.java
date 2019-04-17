a9864211d851d2fd962e781d0e6003bb809eb70f
Kevin Tran Huu <kevin.tranhuu@gmail.com>
2019-04-17 14:38:05 -0400

    Solving conflicts

--- src/main/java/com/revature/caliber/services/AssessmentService.java
+++ src/main/java/com/revature/caliber/services/AssessmentService.java
@@ -216,28 +216,4 @@ public class AssessmentService implements AssessmentServiceInterface{
 	}
 
 
-
-	@Override
-	public List<Assessment> findAssessmentsByBatchIdAndWeekNumber(Integer id, Integer weekNumber) {
-		List<Assessment> AssessmentList = ar.findAssessmentsByBatchIdAndWeekNumber(id, weekNumber);
-		Map<Integer, Boolean> alreadyConnected = new HashMap<>();
-		
-		for(int i = 0; i < AssessmentList.size(); i++) {
-			Assessment a = AssessmentList.get(i);
-			
-			if(!alreadyConnected.containsKey(a.getBatchId())) {
-				if(contactBatchService(a)) {
-					alreadyConnected.put(a.getBatchId(), true);
-				} else {
-					alreadyConnected.put(a.getBatchId(), false);
-				}
-			}
-			
-			if(!alreadyConnected.get(a.getBatchId())) {
-				a.setBatchId(-1);
-			}
-		}
-		return AssessmentList;
-	}
-
 }
