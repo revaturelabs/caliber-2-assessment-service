684bdb3bfd94a3c24d8d05035143072cac50dd79
Sara Martin <martis73@uwosh.edu>
2019-07-26 12:15:53 -0400

    test cases of add assessment

--- src/test/java/com/revature/caliber/repositories/RepositoryTest.java
+++ src/test/java/com/revature/caliber/repositories/RepositoryTest.java
@@ -13,7 +13,6 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.revature.caliber.beans.Assessment;
 import com.revature.caliber.beans.Category;
 
 public class RepositoryTest {
