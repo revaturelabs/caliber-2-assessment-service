f9668c8bf2e64ff9cc6022da0e4b5837063c3f6f
Sara Martin <martis73@uwosh.edu>
2019-07-26 16:08:45 -0400

    category service save

--- src/main/java/com/revature/caliber/repositories/CategoryRepository.java
+++ src/main/java/com/revature/caliber/repositories/CategoryRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.revature.caliber.beans.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
-
-	Category save(Category category);
 	
 	public List<Category> findAllCategories();
 
