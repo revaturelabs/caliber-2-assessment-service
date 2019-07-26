22e51fbd9932867f6db453d0dc44fec678b3056d
Sara Martin <martis73@uwosh.edu>
2019-07-26 12:12:38 -0400

    test cases of add assessment

--- src/test/java/com/revature/caliber/repositories/RepositoryTest.java
+++ src/test/java/com/revature/caliber/repositories/RepositoryTest.java
@@ -1,5 +1,9 @@
 package com.revature.caliber.repositories;
 
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -7,7 +11,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Test;
 
+import com.revature.caliber.beans.Assessment;
 import com.revature.caliber.beans.Category;
 
 public class RepositoryTest {
@@ -31,7 +37,48 @@ public class RepositoryTest {
 		
 		when(categoryRepository.findAllCategories()).thenReturn(categoryList);
 		when(categoryRepository.findCategoryById(3)).thenReturn(c3);
+		when(categoryRepository.findCategoryById(1)).thenReturn(null);
+		when(categoryRepository.findCategoryById(5)).thenReturn(c1);
 		when(categoryRepository.save(c1)).thenReturn(c1);
-		
+		when(categoryRepository.save(c5)).thenReturn(null);
+	}
+	
+	//Testing if things in list
+	@Test
+	public void testFindAllCategoriesMethod() {
+		assertThat(categoryRepository.findAllCategories(), hasItems(
+				new Category(1, "Java", true, "Sara"),
+				new Category(2, "SQL", true, "John"),
+				new Category(3, "Servlets", true, "Ryan"),
+				new Category(4, "Mockito", false, "Jacob")
+		));
+	}
+	
+	//Testing if things in list
+	@Test
+	public void testFindAllCategoriesMethodFail() {
+		assertNotEquals(categoryRepository.findAllCategories(), hasItems(
+				new Category(6, "Spring", false, "Naresh")
+		));
+	}
+	
+	
+	//Check for ID
+	@Test
+	public void testFindId() {
+		assertEquals(null, categoryRepository.findCategoryById(1));
+	}
+	
+	//Create new category
+	@Test
+	public void testCreateCategory() {
+		assertEquals(new Category(1, "Java", true, "Sara"), categoryRepository.save(new Category(1, "Java", true, "Sara")));
+	}
+	
+	//Create same category 
+	@Test
+	public void testCreateCategoryNull() {
+		assertEquals(null, categoryRepository.save(new Category(5, "Java", true, "Josh")));
 	}
 }
+
