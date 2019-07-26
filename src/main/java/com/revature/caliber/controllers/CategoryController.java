8b6e413b7b16c25c8d9fdd6836533035b225a6a2
Ryan Hodder <i_ryan.h@hotmail.com>
2019-07-26 16:20:57 -0400

    Added controller flow
    Added controller method to CategoryController. Also added method to
    service interface.

--- src/main/java/com/revature/caliber/controllers/CategoryController.java
+++ src/main/java/com/revature/caliber/controllers/CategoryController.java
@@ -1,5 +1,38 @@
 package com.revature.caliber.controllers;
 
+import javax.validation.Valid;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.caliber.beans.Category;
+import com.revature.caliber.services.CategoryService;
+
+@RestController
+@CrossOrigin("*")
 public class CategoryController {
 
+private Logger log = Logger.getLogger("AssessmentController.class");
+    
+    @Autowired
+    private CategoryService categoryService;
+	
+	@PutMapping(value="-----------", consumes=MediaType.APPLICATION_JSON_VALUE)
+    @Transactional(isolation=Isolation.READ_COMMITTED, propagation=Propagation.REQUIRED)
+    public ResponseEntity<Category> updateCategory(@Valid @RequestBody Category category) {
+      log.debug("Updating Category: " + category);
+      Category temp = categoryService.updateCategory(category);
+      if(temp == null) return new ResponseEntity<>(temp, HttpStatus.BAD_REQUEST);
+      return new ResponseEntity<>(temp, HttpStatus.OK);
+	}
 }
