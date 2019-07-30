6102cd1e424b670e863a65b5f2aa9c5fa7ceba74
Sara Martin <martis73@uwosh.edu>
2019-07-30 11:19:46 -0400

    Rest API and error classes

--- src/main/java/com/revature/caliber/exceptions/DuplicateException.java
+++ src/main/java/com/revature/caliber/exceptions/DuplicateException.java
@@ -2,6 +2,11 @@ package com.revature.caliber.exceptions;
 
 public class DuplicateException extends RuntimeException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public DuplicateException(String message) {
 		super(message);
 	}
--- src/main/java/com/revature/caliber/repositories/CategoryRepository.java
+++ src/main/java/com/revature/caliber/repositories/CategoryRepository.java
@@ -1,16 +1,10 @@
 package com.revature.caliber.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.revature.caliber.beans.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
-	
-//	public List<Category> findAllCategories();
-
-	//Category findCategoryById(Integer id);
 
 	Category findBySkillCategory(String skillCategory);
 }
