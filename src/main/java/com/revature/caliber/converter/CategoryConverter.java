1a6d78891f84929d0a61cd5b362d096a2e0ff27a
Sara Martin <martis73@uwosh.edu>
2019-07-30 14:08:21 -0400

    dto and converter

---
+++ src/main/java/com/revature/caliber/converter/CategoryConverter.java
@@ -0,0 +1,5 @@
+package com.revature.caliber.converter;
+
+public class CategoryConverter {
+
+}
---
+++ src/main/java/com/revature/caliber/dto/CategoryDTO.java
@@ -0,0 +1,11 @@
+package com.revature.caliber.dto;
+
+public class CategoryDTO {
+
+	public CategoryDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
