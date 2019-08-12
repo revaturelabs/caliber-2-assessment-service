cf307ebd2673381682db7ec42c89d11897f4cb55
dkhalifa95 <doaak.k95@gmail.com>
2019-08-12 11:30:02 -0400

    headless now

--- src/test/java/com/revature/endtoend/NavigationAtoQ.java
+++ src/test/java/com/revature/endtoend/NavigationAtoQ.java
@@ -18,24 +18,51 @@ import org.testng.annotations.Test;
  */
 
 public class NavigationAtoQ {
+    
+	protected WebDriver driver; 
+	
+	//public final String URL = System.getenv("CALIBER_BASE_URL") + "/caliber/vp/audit";
 
-	protected WebDriver driver;
+	//private String URL = System.getenv("CALIBER_BASE_URL") + "/caliber/vp/assess";
 
-	@Test
+//	@BeforeClass
+//	public void setup() {
+//		String OS;
+//		OS = System.getProperty("os.name");
+//		if (OS.startsWith("Windows")) {
+//			System.setProperty("webdriver.chrome.driver", "src/test/drivers/chromedriver.exe");
+//		}else { if(OS.startsWith("Mac"))
+//			System.setProperty("webdriver.chrome.driver", "src/test/drivers/chromedriver");
+//		else {
+//			System.setProperty("webdriver.chrome.driver","src/test/drivers/chromedriverLinux");
+//		}
+//		}
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--headless");
+//		driver = new ChromeDriver(options);
+//		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+//		driver.get(URL);
+//	}
+	
+	@Test (enabled=false)
 	public void assessBatch() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\doaak\\Desktop\\Project3\\caliber-2-meta\\assessment-service\\src\\test\\drivers\\chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("headless");
-		options.addArguments("window-size=1200x600");
-		WebDriver driver = new ChromeDriver(options);
-		driver.get("http://localhost:4200/caliber/vp/assess");
-
-		System.out.println("title is: " + driver.getTitle());
+		//System.setProperty("webdriver.chrome.driver",
+				//"C:\\Users\\doaak\\Desktop\\Project3\\caliber-2-meta\\quality-audit-service\\Drivers\\chromedriver.exe");
+		//driver = new ChromeDriver();
+	     System.setProperty("webdriver.chrome.driver",
+                 "C:\\Users\\doaak\\Desktop\\Project3\\caliber-2-meta\\assessment-service\\src\\test\\drivers\\chromedriver.exe");
+         ChromeOptions options = new ChromeOptions();
+         options.addArguments("headless");
+         options.addArguments("window-size=1200x600");
+         WebDriver driver = new ChromeDriver(options);
+         driver.get("http://localhost:4200/caliber/vp/assess");
+         //driver.get("https://www.google.co.in/");
+         System.out.println("title is: " + driver.getTitle());
 		AssessBatchPOM assessBatchPOM = new AssessBatchPOM(driver);
 		driver.manage().timeouts().implicitlyWait(30000, TimeUnit.MILLISECONDS);
 		String url = "http://localhost:4200/caliber/vp/assess";
 		String url1 = "";
+		//driver.get("http://localhost:4200/caliber/vp/assess");
 
 		url1 = driver.getCurrentUrl();
 
@@ -46,22 +73,26 @@ public class NavigationAtoQ {
 		}
 
 	}
-
+	
 	@Test
 	public void assessBatchNav() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\doaak\\Desktop\\Project3\\caliber-2-meta\\assessment-service\\src\\test\\drivers\\chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("headless");
-		options.addArguments("window-size=1200x600");
-		WebDriver driver = new ChromeDriver(options);
-		driver.get("http://localhost:4200/caliber/vp/assess");
-		// driver.get("https://www.google.co.in/");
-		System.out.println("title is: " + driver.getTitle());
+	
+		 System.setProperty("webdriver.chrome.driver",
+                 "C:\\Users\\doaak\\Desktop\\Project3\\caliber-2-meta\\assessment-service\\src\\test\\drivers\\chromedriver.exe");
+         ChromeOptions options = new ChromeOptions();
+         options.addArguments("headless");
+         options.addArguments("window-size=1200x600");
+         WebDriver driver = new ChromeDriver(options);
+         driver.get("http://localhost:4200/caliber/vp/assess");
+         //driver.get("https://www.google.co.in/");
+         System.out.println("title is: " + driver.getTitle());
 		AssessBatchPOM assessBatchPOM = new AssessBatchPOM(driver);
-		String url = "http://localhost:4200/caliber/vp/assess";
+		String url = "http://localhost:4200/caliber/vp/audit";
+//		String url1 = "";
 		driver.findElement(By.id("quality-link")).click();
-		assertEquals(driver.getCurrentUrl(), url);
+		assertEquals(driver.getCurrentUrl(),url);
 
-	}
+    
 }
+}
+
