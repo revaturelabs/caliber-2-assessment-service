3d5d6694feb11c6d037129f9792541b2709fd053
RevatureGentry <william.gentry@revature.com>
2019-12-10 17:58:20 -0500

    Named @FeignClient to suppress exception on startup

--- src/main/java/com/revature/caliber/intercoms/perf/BatchClientPerf.java
+++ src/main/java/com/revature/caliber/intercoms/perf/BatchClientPerf.java
@@ -8,6 +8,6 @@ import org.springframework.context.annotation.Profile;
  * @author William Gentry
  */
 @Profile("perf")
-@FeignClient(url = "http://batch.caliber-2-perf")
+@FeignClient(name = "batch-service", url = "http://batch.caliber-2-perf")
 public interface BatchClientPerf extends BatchClient  {
 }
