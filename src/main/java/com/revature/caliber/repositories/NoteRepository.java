714b64e79cd4c95d96c66ac3aa989501d3b6cdc5
Wyatt LaVoie <wyatt.lavoie@revature.net>
2019-12-19 16:27:40 -0500

    replace more Wrappers with primitives

--- src/main/java/com/revature/caliber/repositories/NoteRepository.java
+++ src/main/java/com/revature/caliber/repositories/NoteRepository.java
@@ -10,10 +10,10 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface NoteRepository extends JpaRepository<Note, Integer>{
 	
-	public List<Note> findNotesByTraineeId(Integer id);
-	public List<Note> findNotesByBatchId(Integer id);
-	public List<Note> findNotesByBatchIdAndWeekNumber(Integer bid, Integer weekNum);
+	public List<Note> findNotesByTraineeId(int id);
+	public List<Note> findNotesByBatchId(int id);
+	public List<Note> findNotesByBatchIdAndWeekNumber(int bid, int weekNum);
 
 	@Query("SELECT n FROM Note n WHERE n.batchId = ?1 and n.weekNumber = ?2 and n.noteType = 'BATCH'")
-	Optional<Note> findBatchNoteByBatchIdAndWeekNumber(Integer batchId, Integer week);
+	Optional<Note> findBatchNoteByBatchIdAndWeekNumber(int batchId, int week);
 }
